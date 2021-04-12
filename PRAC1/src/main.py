4708b82ef0538388658212ebb304a491a2cee5c5
Daniel Orbegoso <daniel.orbegoso@lhind.dlh.de>
2021-04-12 22:11:34 +0200

    new foler src created

---
+++ PRAC1/src/main.py
@@ -0,0 +1,9 @@
+from src.scraper import coronaWorldometersScrapper
+
+output_file = "statsCorona.csv"
+
+#We are calling here the main function, receiving a df with the information and storing it in a csv.
+
+scraper = coronaWorldometersScrapper()
+df = scraper.scrap()
+scraper.df_to_csv(output_file,df)
